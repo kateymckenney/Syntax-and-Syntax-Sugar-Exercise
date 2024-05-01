16ca91327bcaa2490506156be8581e35b20e7092
Katey McKenney <mckenneykatey@gmail.com>
2024-05-01 14:50:53 -0500

    completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+
+            Console.WriteLine(response);
+
         }
     }
 }
